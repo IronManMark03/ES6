8ef28004393a28e70c610b406c4b1e37ee750f55
IronManMark03 <IMANMarkIII@gmail.com>
2021-12-08 15:58:44 +0530

    es6 number

--- main.js
+++ main.js
@@ -1,15 +1,1 @@
-//alert('main js file');
-console.log('hello');
-
-// in ES 6 we are using let, const
-
-let letage = 30;
-letage = 31;
-
-console.log(letage);
-
-const constAge = 44;
-
-
-
-console.log(constAge)
+// string, Number , Boolean , Null, Undefined
