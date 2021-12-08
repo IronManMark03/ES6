9067ef64f8fc2150b5e80ffc5d1318bc798b91e0
IronManMark03 <IMANMarkIII@gmail.com>
2021-12-08 16:04:37 +0530

    adding numbers

--- main.js
+++ main.js
@@ -1,1 +1,9 @@
 // string, Number , Boolean , Null, Undefined
+
+const name = 'Bharath';
+const age = '38';
+const rating = '4.5';
+const isCool = false;
+const x = null;
+const y = undefined;
+let z;
