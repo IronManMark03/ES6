afe2aa7c4e272257a9d96f60feb23faa388cdd6e
IronManMark03 <IMANMarkIII@gmail.com>
2021-12-09 17:29:12 +0530

    array

--- main.js
+++ main.js
@@ -27,3 +27,21 @@ console.log(s.split(''));
 const newString = 'technology, computers, it, code';
 
 console.log(newString.split(','))
+
+//Arrays -variables that hold multiple values
+
+const numberArray = new Array(1, 2, 3, 4, 5, 6, 7);
+
+console.log(numberArray);
+
+const fruits = ['apples', 'oranges', 'pears'];
+
+console.log(fruits);
+
+console.log(fruits[1]);
+
+fruits[3] = 'new';
+
+console.log(fruits);
+
+console.log(fruits.indexOf('oranges'));
