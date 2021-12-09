4b1acb0ebfd37f5886afdfeb4de00b21a7f54e8b
IronManMark03 <IMANMarkIII@gmail.com>
2021-12-09 17:14:20 +0530

    adding String

--- main.js
+++ main.js
@@ -7,3 +7,23 @@ const isCool = false;
 const x = null;
 const y = undefined;
 let z;
+
+// String
+
+const s = 'Hello World!';
+
+console.log(s.length);
+
+console.log(s.toUpperCase());
+
+console.log(s.toLowerCase());
+
+console.log(s.substring(0, 5));
+
+console.log(s.substring(0, 5).toUpperCase());
+
+console.log(s.split(''));
+
+const newString = 'technology, computers, it, code';
+
+console.log(newString.split(','))
