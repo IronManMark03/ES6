3574d19c4a83a69ff78985ecf00df5cd1da8d961
IronManMark03 <IMANMarkIII@gmail.com>
2021-12-10 15:45:11 +0530

    updated

--- main.js
+++ main.js
@@ -45,3 +45,41 @@ fruits[3] = 'new';
 console.log(fruits);
 
 console.log(fruits.indexOf('oranges'));
+
+//Object
+
+const person = {
+    firstName: 'Bharath',
+    lastName: 'Kumar',
+    age: '38',
+    hobbies: ['music', 'movies', 'sport'],
+    address: {
+        street: 'KHB colony',
+        city: 'Bangalore',
+        state: 'Karanataka'
+    }
+}
+
+console.log(person);
+
+console.log(person.firstName);
+
+console.log(person.hobbies[0]);
+
+console.log(person.address.state);
+
+const {
+    firstName,
+    lastName,
+    address: {
+        city
+    }
+} = person
+
+console.log(lastName);
+
+console.log(city);
+
+person.email='bharathabk@gmail.com';
+
+console.log(person);
